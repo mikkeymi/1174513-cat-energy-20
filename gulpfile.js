1432b00ebfde244041b8560d39abca59444c2af3
Anastasia <sweetmikkey@mail.ru>
2020-09-16 19:36:54 +0400

    проект

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,7 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
